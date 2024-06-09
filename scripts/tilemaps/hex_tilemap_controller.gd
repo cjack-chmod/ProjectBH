1605dae1242b6d0118aa3a2c086067fcdc25a409
cjack-chmod <75388979+cjack-chmod@users.noreply.github.com>
2024-06-09 16:30:34 +0200

    basic grid selecter

---
+++ scripts/tilemaps/hex_tilemap_controller.gd
@@ -0,0 +1,25 @@
+extends TileMap
+
+const main_layer = 0
+const main_atlas_id = 0
+const this_was_great = "yup"
+
+func _input(event):
+	if event is InputEventMouseButton:
+		# If LMB Pressed
+		if event.button_index == MOUSE_BUTTON_LEFT and event.is_pressed():
+			# Finds local position of mouse
+			var global_clicked = get_local_mouse_position()
+			# Translates to which map coord it is in
+			var pos_clicked = local_to_map(to_local(global_clicked))
+
+			# Finds the cell in the atlas and its current alt
+			var current_atlas_coords = get_cell_atlas_coords(main_layer, pos_clicked)
+			var current_tile_alt = get_cell_alternative_tile(main_layer, pos_clicked)
+
+			# Increments the alternative (loop since there can be multiple alts )
+			if current_tile_alt > -1:
+				var number_of_alts_for_clicked = tile_set.get_source(main_atlas_id)\
+						.get_alternative_tiles_count(current_atlas_coords)
+				set_cell(main_layer, pos_clicked, main_atlas_id, current_atlas_coords, 
+						(current_tile_alt + 1) %  number_of_alts_for_clicked)
