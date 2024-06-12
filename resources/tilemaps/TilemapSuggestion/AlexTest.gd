794d6aed07be173b4c31bef68462a2a8489b4534
Alex <143654953+Cityships@users.noreply.github.com>
2024-06-12 17:02:37 -0400

    Tileset suggestions, see tilemap suggestions folder and 44x32 tilemap.tscn

--- resources/tilemaps/TilemapSuggestion/AlexTest.gd
+++ resources/tilemaps/TilemapSuggestion/AlexTest.gd
@@ -2,5 +2,5 @@ extends Node
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	print("hello")
+	print(get_parent().name + ": AlexTest.gd, test sample")
 	pass # Replace with function body.
