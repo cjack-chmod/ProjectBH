4d47a22a912d6e7636c967db018b4ece0113a59f
Alex <143654953+Cityships@users.noreply.github.com>
2024-07-01 14:47:23 -0400

    Under tilemaps "suggestion tilemap" contains presets

---
+++ resources/tilemaps/TilemapSuggestion/AlexTest.gd
@@ -0,0 +1,6 @@
+extends Node
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	print(get_parent().name + ": AlexTest.gd, test sample")
+	pass # Replace with function body.
